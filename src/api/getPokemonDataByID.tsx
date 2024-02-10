7281201ffb87c364750f324878c9bd8176891f56
galeonn321 <santichiste@hotmail.com>
2024-02-10 17:22:13 +0100

    small change

--- src/api/getPokemonDataByID.tsx
+++ src/api/getPokemonDataByID.tsx
@@ -2,7 +2,7 @@ import { LOG } from "../config/logger";
 
 
 //This function gets precise data by ID to get thei abilities, type and much more and put what I need into an object.
-export const usePokemon = async (id) => {
+export const getPokemonDataByID = async (id) => {
   const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   const data = await resp.json();
 
@@ -16,5 +16,5 @@ export const usePokemon = async (id) => {
     abilities: abilities,
   };
 
-  return pokemonObject;
+  return getPokemonDataByID;
 };
