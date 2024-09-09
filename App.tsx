9a53eb4b6c8874666f3a1d01a30579cf8efc6911
galeonn321 <santichiste@hotmail.com>
2024-09-09 13:21:41 +0200

    upgrade to latest

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 import React from "react";
 import HomeScreen from "./src/screens/HomeScreen";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <HomeScreen>
-        <StatusBar style="auto" />
-      </HomeScreen>
+      <HomeScreen />
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
